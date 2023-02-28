1a17e578e7d6418ab092caa6426540ae703d1453
Owdeer <101393267+Owdeer@users.noreply.github.com>
2023-02-28 20:41:12 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.Objects;
+public class Main {
+    public static void main(String[] args) {
+        Scanner math = new Scanner(System.in);
+        String answer = math.nextLine();
+        calc(answer);
+    }
+
+    public static String calc(String input) {
+        var massiveEq = input.split(" ");
+        int length = massiveEq.length;
+        if (length != 3) {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("throws Exception");
+            }
+            System.exit(0);
+        }
+        int a = Integer.parseInt(massiveEq[0]);
+        String c = massiveEq[1];
+        int b = Integer.parseInt(massiveEq[2]);
+        if (a < 1 | a > 10 | b < 1 | b > 10) {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("throws Exception");
+            }
+            System.exit(0);
+        }
+
+        switch (c) {
+            case ("+"):
+                int answerEqPlus = a + b;
+                System.out.println(answerEqPlus);
+                break;
+            case ("-"):
+                int answerEqMinus = a - b;
+                System.out.println(answerEqMinus);
+                break;
+            case ("*"):
+                int answerEqMult = a * b;
+                System.out.println(answerEqMult);
+                break;
+            case ("/"):
+                int answerEqDiv = a / b;
+                System.out.println(answerEqDiv);
+                break;
+            default: try {
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("throws Exception");
+            }
+            System.exit(0);
+        }
+        return input;
+    }
+}
