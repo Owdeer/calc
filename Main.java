56ebd2dd766e376808b9bac990770464117c9069
Owdeer <101393267+Owdeer@users.noreply.github.com>
2023-03-03 16:07:24 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner math = new Scanner(System.in);
         String answer = math.nextLine();
-        calc(answer);
+        System.out.println(calc(answer));
     }
 
     public static String calc(String input) {
-        var massiveEq = input.split(" ");
+        String[] massiveEq = input.split(" ");
         int length = massiveEq.length;
         if (length != 3) {
             try {
@@ -18,7 +18,7 @@ public class Main {
             }
             System.exit(0);
         }
-        try {//try
+        try {
             int a = Integer.parseInt(massiveEq[0]);
             int b = Integer.parseInt(massiveEq[2]);
             if (a < 1 | a > 10 | b < 1 | b > 10) {
@@ -30,33 +30,36 @@ public class Main {
                 System.exit(0);
             }
             String c = massiveEq[1];
+
             switch (c) {
                 case ("+"):
-                    int answerEqPlus = a + b;
-                    System.out.println(answerEqPlus);
+                    int answerPlus = a + b;
+                    input = String.valueOf(answerPlus);
                     break;
                 case ("-"):
-                    int answerEqMinus = a - b;
-                    System.out.println(answerEqMinus);
+                    int answerMinus = a - b;
+                    input = String.valueOf(answerMinus);
                     break;
                 case ("*"):
-                    int answerEqMult = a * b;
-                    System.out.println(answerEqMult);
+                    int answerMult = a * b;
+                    input = String.valueOf(answerMult);
                     break;
                 case ("/"):
-                    int answerEqDiv = a / b;
-                    System.out.println(answerEqDiv);
+                    int answerDiv = a / b;
+                    input = String.valueOf(answerDiv);
                     break;
                 default: try {
                     throw new IOException();
                 } catch (IOException e) {
                     System.out.println("throws Exception");
                 }
-                System.exit(0);
+                    break;
             }
         } catch (NumberFormatException e) {
             System.out.println("throws Exception");
-        }// catch
+            System.exit(0);
+        }
         return input;
     }
+
 }
